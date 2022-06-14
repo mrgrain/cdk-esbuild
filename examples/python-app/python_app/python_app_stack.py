932f7932663c0b53869f654a9ab52a4fa12a398a
Momo Kornher <mail@moritzkornher.de>
2022-06-14 17:08:06 +1200

    docs(examples/python-app): one example to rule them all

--- examples/python-app/python_app/python_app_stack.py
+++ examples/python-app/python_app/python_app_stack.py
@@ -1,12 +1,31 @@
 from aws_cdk import Stack
 import aws_cdk.aws_lambda as lambda_
+import aws_cdk.aws_s3 as s3
+import aws_cdk.aws_s3_deployment as s3_deployment
 from constructs import Construct
-from mrgrain.cdk_esbuild import TypeScriptCode, BuildOptions
+from mrgrain.cdk_esbuild import (
+    BuildOptions,
+    InlineTypeScriptCode,
+    TypeScriptCode,
+    TypeScriptSource,
+)
 
 
 class PythonAppStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        s3_deployment.BucketDeployment(
+            self,
+            "Website",
+            sources=[
+                TypeScriptSource("lambda-handler/index.ts", copy_dir="lambda-handler")
+            ],
+            destination_bucket=s3.Bucket(
+                self,
+                "Bucket",
+            ),
+        )
 
         lambda_.Function(
             self,
@@ -18,5 +37,20 @@ class PythonAppStack(Stack):
                 build_options=BuildOptions(
                     format="esm", outfile="index.mjs", external=["aws-sdk"]
                 ),
+            ),
+        )
+
+        lambda_.Function(
+            self,
+            "InlineFunction",
+            runtime=lambda_.Runtime.NODEJS_16_X,
+            handler="index.handler",
+            code=InlineTypeScriptCode(
+                """
+                const hello: string = 'world';
+                export function handler() {
+                    console.log(hello);
+                }
+                """
             ),
         )
