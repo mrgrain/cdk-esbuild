b4ce20b818864b5d9a2d9b8537552187280bf7ec
Matthias Pichler <m.pichler@warrify.com>
2022-07-15 11:38:31 +0200

    fix: remove extra whitespace
    Co-authored-by: Momo Kornher <mail@moritzkornher.de>

--- src/bundler.ts
+++ src/bundler.ts
@@ -198,7 +198,7 @@ export class EsbuildBundler {
         /**
          * only reset `ESBUILD_BINARY_PATH` if it was explicitly set via the construct props
          * since `esbuild` itself sometimes sets it (eg. when running in yarn 2 plug&play)
-         */ 
+         */
         if (this.props.esbuildBinaryPath) {
           process.env.ESBUILD_BINARY_PATH = originalEsbuildBinaryPath;
         }
