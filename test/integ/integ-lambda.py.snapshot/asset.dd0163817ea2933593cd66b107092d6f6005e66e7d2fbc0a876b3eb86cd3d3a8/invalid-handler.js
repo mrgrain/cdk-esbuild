d4c352ed1c1ae44aa046dc0fe0a8032e4fe1aff9
Momo Kornher <mail@moritzkornher.de>
2023-01-03 21:04:57 +0100

    fix integ tests

--- test/integ/integ-lambda.py.snapshot/asset.dd0163817ea2933593cd66b107092d6f6005e66e7d2fbc0a876b3eb86cd3d3a8/invalid-handler.js
+++
@@ -1,5 +0,0 @@
-import { invalid } from './util';
-
-export async function handler(): Promisevoid> {
-  consoe.lmult(3, 4)); // eslint-disable-line no-console
-}
--- test/integ/integ-website.py
+++ test/integ/integ-website.py
@@ -5,19 +5,12 @@ import aws_cdk.integ_tests_alpha as integ
 from constructs import Construct
 from mrgrain.cdk_esbuild import (
     TypeScriptSource,
-    EsbuildProvider,
-    EsbuildSource,
 )
 
 
 class WebsiteStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # Set a new default
-        EsbuildProvider.override_default_provider(
-            EsbuildProvider(esbuild_module_path=EsbuildSource.install())
-        )
 
         s3_deployment.BucketDeployment(
             self,
@@ -26,7 +19,6 @@ class WebsiteStack(cdk.Stack):
                 TypeScriptSource(
                     "../fixtures/handlers/colors.ts",
                     copy_dir="../fixtures/handlers",
-                    build_provider=EsbuildProvider(),  # Override for a specific construct
                 )
             ],
             destination_bucket=s3.Bucket(
