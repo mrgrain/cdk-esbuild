18b6de243f11932c64ecbc86e0194ba3589d37e3
Momo Kornher <mail@moritzkornher.de>
2023-01-07 18:25:06 +0100

    docs(examples/typescript): update to v4

--- examples/typescript/esbuild-with-plugins/app.ts
+++ examples/typescript/esbuild-with-plugins/app.ts
@@ -2,28 +2,32 @@
 import { App, CfnOutput, Stack, StackProps } from "aws-cdk-lib";
 import { Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
-import { BuildOptions, TypeScriptCode } from "@mrgrain/cdk-esbuild";
-import { execSync } from "child_process";
-import { BuildResult } from "esbuild";
+import { IBuildProvider, ProviderBuildOptions, TypeScriptCode } from "@mrgrain/cdk-esbuild";
+import { spawnSync } from "child_process";
+
+class BuildScriptProvider implements IBuildProvider {
+  constructor(public readonly scriptPath: string) { }
+
+  buildSync(options: ProviderBuildOptions): void {
+    const result = spawnSync('node', [this.scriptPath, JSON.stringify(options)], {
+      stdio: ['inherit', 'inherit', 'pipe'],
+    });
+
+    if (result.stderr.byteLength > 0) {
+      throw result.stderr.toString();
+    }
+  }
+}
 
 export class LambdaStack extends Stack {
   constructor(scope?: Construct, id?: string, props?: StackProps) {
     super(scope, id, props);
 
     const lambda = new Function(this, "Lambda", {
-      runtime: Runtime.NODEJS_16_X,
+      runtime: Runtime.NODEJS_18_X,
       handler: "lambda.handler",
       code: new TypeScriptCode("./lambda.ts", {
-        buildFn: (options: BuildOptions): BuildResult => {
-          try {
-            execSync(`node build.mjs '${JSON.stringify(options)}'`, {
-              stdio: "inherit",
-            });
-            return { errors: [], warnings: [] };
-          } catch (error) {
-            throw { errors: [], warnings: [] };
-          }
-        },
+        buildProvider: new BuildScriptProvider('build.mjs')
       }),
     });
 
