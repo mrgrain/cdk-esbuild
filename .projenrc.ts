bb8daba3823e73d2cbba053068288029f00a3b7d
Momo Kornher <mail@moritzkornher.de>
2023-01-08 15:05:21 +0000

    docs: use gray wordmark for readmes

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,6 +2,7 @@ import { awscdk, github, javascript, release, vscode } from 'projen';
 import { SourceFile } from 'ts-morph';
 import { tagOnNpm, TypeScriptSourceFile } from './projenrc';
 import { IntegrationTests } from './projenrc/IntegrationTests';
+import { WordmarkReadme } from './projenrc/WordmarkReadme';
 import { Esbuild } from './src/private/esbuild-source';
 
 const project = new awscdk.AwsCdkConstructLibrary({
@@ -220,13 +221,8 @@ project.addGitIgnore('.jsii.tabl.json');
 project.addPackageIgnore('.jsii.tabl.json');
 project.addPackageIgnore('/rosetta/');
 
-// pypi release
-const wordmark = '<img src="https://raw.githubusercontent.com/mrgrain/cdk-esbuild/main/images/wordmark-light.svg" alt="cdk-esbuild">';
-const readme = project.tasks.addTask('prepare:readme', {
-  exec: `sed -i -e '1,5d' -e '6i ${wordmark}' README.md`,
-});
-project.tasks.tryFind('compile')?.prependExec(`if [ ! -z \${CI} ]; then npx projen ${readme.name}; fi`);
-project.tasks.tryFind('compile')?.exec('if [ ! -z ${CI} ]; then git checkout README.md; fi');
+// Wordmark images in README
+new WordmarkReadme(project, { altText: 'cdk-esbuild' });
 
 // eslint
 project.eslint?.addRules({
