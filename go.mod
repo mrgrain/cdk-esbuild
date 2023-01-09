a15968cdb56a3f91c96e3eb66bbcfe3f32ccefe4
Momo Kornher <mail@moritzkornher.de>
2023-01-09 13:29:49 +0000

    chore: go integ tests (#344)

---
+++ go.mod
@@ -0,0 +1,19 @@
+module cdkesbuild_integ_tests
+
+go 1.16
+
+require github.com/aws/aws-cdk-go/awscdk/v2 v2.58.1
+
+require github.com/aws/aws-cdk-go/awscdkintegtestsalpha/v2 v2.58.1-alpha.0
+
+require (
+	github.com/aws/constructs-go/constructs/v10 v10.1.189
+	github.com/aws/jsii-runtime-go v1.73.0
+	github.com/mrgrain/cdk-esbuild-go/cdkesbuild v0.0.0-unpublished
+)
+
+replace github.com/mrgrain/cdk-esbuild-go/cdkesbuild v0.0.0-unpublished => ./dist/go/cdkesbuild
+
+replace github.com/mrgrain/cdk-esbuild-go/cdkesbuild/jsii v0.0.0-unpublished => ./dist/go/cdkesbuild/jsii
+
+replace github.com/mrgrain/cdk-esbuild-go/cdkesbuild/internal v0.0.0-unpublished => ./dist/go/cdkesbuild/internal
--- projenrc/IntegrationTests.ts
+++ projenrc/IntegrationTests.ts
@@ -13,7 +13,7 @@ export interface IntegrationTestsOptions {
   readonly testdir?: string;
 
   /**
-   * Options for Python integ tests
+   * Options for Python integration tests
    */
   readonly python?: {
     /**
@@ -22,18 +22,53 @@ export interface IntegrationTestsOptions {
      * @default - CDK version from project
      */
     cdkVersion?: string;
+
+    /**
+     * The pattern used to detect python integration tests.
+     *
+     * @default "^integ-.*\\.py$"
+     */
+    testPattern?: string;
+  };
+
+  /**
+   * Options for Go integration tests
+   */
+  readonly go?: {
+    /**
+     * The version of CDK to use for the integration test.
+     *
+     * @default - CDK version from project
+     */
+    cdkVersion?: string;
+
+    /**
+     * The pattern used to detect Go integration tests.
+     *
+     * @default "^integ_.*\\.go$"
+     */
+    testPattern?: string;
   };
 }
 
 export class IntegrationTests extends Component {
-  public constructor(project: awscdk.AwsCdkConstructLibrary, options: IntegrationTestsOptions) {
+  private pythonPattern: string;
+  private goPattern: string;
+
+  public constructor(
+    public readonly project: awscdk.AwsCdkConstructLibrary,
+    private readonly options: IntegrationTestsOptions,
+  ) {
     super(project);
 
-    const pythonPattern = '^integ-.*\\.py$';
+    // Define patterns
+    this.pythonPattern = options.python?.testPattern ?? '^integ-.*\\.py$';
+    this.goPattern = options.go?.testPattern ?? '^integ_.*\\.go$';
 
     const cwd = join(this.project.outdir, options.testdir ?? project.testdir);
     const hasTestFor = matchDir(cwd, {
-      python: new RegExp(pythonPattern),
+      python: new RegExp(this.pythonPattern),
+      go: new RegExp(this.goPattern),
     });
 
     // No tests found => Abort early
@@ -45,47 +80,115 @@ export class IntegrationTests extends Component {
     project.addDevDeps('@aws-cdk/integ-runner@^2');
     project.addGitIgnore('cdk-integ.out.*');
 
-    // Python
-    const pythonTarget = project.package.manifest?.jsii?.targets.python;
-    if (pythonTarget && hasTestFor.python) {
-      // Task
-      const pythonInteg = project.addTask('integ:python', {
-        exec: `integ-runner --app="pipenv run python {filePath}" --test-regex="${pythonPattern}"`,
-        receiveArgs: true,
-      });
-      pythonInteg.prependExec('pipenv install --skip-lock');
+    // Langugages
+    const jsiiTargets = project.package.manifest?.jsii?.targets;
 
-      // Workflow
-      project.buildWorkflow?.addPostBuildJobCommands('integ-python', [
-        'pip install pipenv',
-        'mv dist .repo',
-        'cd .repo',
-        'npm ci',
-        'npx projen package:python',
-        'npx projen integ:python',
-      ], {
-        tools: {
-          python: {
-            version: '3.x',
-          },
-          node: {
-            version: project.minNodeVersion ?? '14.x',
-          },
-        },
-      });
-
-      // Pipenv
-      project.addPackageIgnore('Pipfile');
-      project.addPackageIgnore('Pipfile.lock');
-      project.addGitIgnore('Pipfile.lock');
-
-      const pythonCdkVersion = options.python?.cdkVersion ?? project.cdkVersion;
-      const pipenv = new Pipenv(project);
-      pipenv.addPackage(`aws-cdk-lib@==${pythonCdkVersion}`);
-      pipenv.addPackage(`aws-cdk.integ-tests-alpha@==${pythonCdkVersion}a0`);
-      pipenv.addPackage('constructs@<11.0.0,>=10.0.0');
-      pipenv.addPackage(`${pythonTarget.distName}@{path = "./dist/python/${pythonTarget.module}-0.0.0-py3-none-any.whl"}`);
+    if (jsiiTargets.python && hasTestFor.python) {
+      this.setupPython(jsiiTargets.python);
     }
+
+    if (jsiiTargets.go && hasTestFor.go) {
+      this.setupGo(jsiiTargets.go);
+    }
+  }
+
+  private setupPython(pythonTarget: {
+    distName: string;
+    module: string;
+  }) {
+    // Task
+    const pythonInteg = this.project.addTask('integ:python', {
+      exec: `integ-runner --app="pipenv run python {filePath}" --test-regex="${this.pythonPattern}"`,
+      receiveArgs: true,
+    });
+    pythonInteg.prependExec('pipenv install --skip-lock');
+
+    // Workflow
+    this.project.buildWorkflow?.addPostBuildJobCommands('integ-python', [
+      'pip install pipenv',
+      'mv dist .repo',
+      'cd .repo',
+      'npm ci',
+      'npx projen package:python',
+      'npx projen integ:python',
+    ], {
+      tools: {
+        python: {
+          version: '3.x',
+        },
+        node: {
+          version: this.project.minNodeVersion ?? '14.x',
+        },
+      },
+    });
+
+    // Pipenv
+    this.project.addPackageIgnore('Pipfile');
+    this.project.addPackageIgnore('Pipfile.lock');
+    this.project.addGitIgnore('Pipfile.lock');
+
+    const pythonCdkVersion = this.options.python?.cdkVersion ?? this.project.cdkVersion;
+    const pipenv = new Pipenv(this.project);
+    pipenv.addPackage(`aws-cdk-lib@==${pythonCdkVersion}`);
+    pipenv.addPackage(`aws-cdk.integ-tests-alpha@==${pythonCdkVersion}a0`);
+    pipenv.addPackage('constructs@<11.0.0,>=10.0.0');
+    pipenv.addPackage(`${pythonTarget.distName}@{path = "./dist/python/${pythonTarget.module}-0.0.0-py3-none-any.whl"}`);
+  }
+
+  private setupGo(_goTarget: {
+    moduleName: string;
+    packageName?: string;
+  }) {
+    const goVersion = '1.16';
+    // const goCdkVersion = this.options.go?.cdkVersion ?? this.project.cdkVersion.replace(/[\^~]+/g, '');
+    // const goPackageName = goTarget.packageName ?? this.project.name.replace(/[\W_]+/g, '');
+    // const goRepository = goTarget.moduleName;
+
+    // Task
+    this.project.addTask('integ:go', {
+      exec: `integ-runner --app="go run {filePath}" --test-regex="${this.goPattern}"`,
+      receiveArgs: true,
+    });
+
+    // Workflow
+    this.project.buildWorkflow?.addPostBuildJobCommands('integ-go', [
+      'mv dist .repo',
+      'cd .repo',
+      'npm ci',
+      'npx projen package:go',
+      'npx projen integ:go',
+    ], {
+      tools: {
+        go: {
+          version: `^${goVersion}.0`,
+        },
+        node: {
+          version: this.project.minNodeVersion ?? '14.x',
+        },
+      },
+    });
+
+    // go.mod
+    this.project.addPackageIgnore('go.mod');
+    this.project.addPackageIgnore('go.sum');
+
+    // new TextFile(this.project, 'go.mod', {
+    //   marker: true,
+    //   readonly: false,
+    //   lines: [
+    //     `// ${PROJEN_MARKER}`,
+    //     `module ${goPackageName}_integ_tests`,
+    //     '',
+    //     `go ${goVersion}`,
+    //     '',
+    //     `require github.com/aws/aws-cdk-go/awscdk/v2 v${goCdkVersion}`,
+    //     `require github.com/aws/aws-cdk-go/awscdkintegtestsalpha/v2 v${goCdkVersion}-alpha.0`,
+    //     `require ${goRepository}/${goPackageName} v0.0.0-unpublished`,
+    //     `replace ${goRepository}/${goPackageName} v0.0.0-unpublished => ./dist/go/${goPackageName}`,
+    //     `replace ${goRepository}/${goPackageName}/jsii v0.0.0-unpublished => ./dist/go/${goPackageName}/jsii`,
+    //     `replace ${goRepository}/${goPackageName}/internal v0.0.0-unpublished => ./dist/go/${goPackageName}/internal`,
+    //   ],
+    // });
   }
 }
 
