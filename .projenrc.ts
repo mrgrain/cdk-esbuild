0293c32ce1fcd3d2301adf7677246aa67605e768
Momo Kornher <mail@moritzkornher.de>
2023-01-30 12:35:22 +0000

    chore: auto-release v4 twice a month, v3 once a month

--- .projenrc.ts
+++ .projenrc.ts
@@ -71,9 +71,9 @@ const project = new awscdk.AwsCdkConstructLibrary({
       npmDistTag: 'old-stable',
     },
   },
-  releaseTrigger: {
-    isContinuous: false,
-  } as release.ReleaseTrigger,
+  releaseTrigger: release.ReleaseTrigger.scheduled({
+    schedule: '0 5 1,15 * *',
+  }),
   publishToPypi: {
     distName: 'mrgrain.cdk-esbuild',
     module: 'mrgrain.cdk_esbuild',
@@ -212,7 +212,7 @@ v3ReleaseWorkflow?.addToArray('jobs.release.steps', {
   name: 'Publish Changelog',
   run: 'npx projen publish:git:v3',
 });
-v3ReleaseWorkflow?.addToArray('on.schedule', { cron: '0 5 * * 1' });
+v3ReleaseWorkflow?.addOverride('on.schedule', [{ cron: '0 5 15 * *' }]);
 v3ReleaseWorkflow?.addOverride('jobs.release.steps.0.with.ref', 'v3');
 v3ReleaseWorkflow?.addOverride('jobs.release_golang.steps.10.env.GIT_BRANCH', 'v3');
 
