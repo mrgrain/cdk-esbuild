61ef3920f6ea894c3e522a832d501c8088184b62
Momo Kornher <mail@moritzkornher.de>
2025-10-05 15:43:50 +0100

    fixup

--- src/cloudfront-function-code.ts
+++ src/cloudfront-function-code.ts
@@ -209,7 +209,7 @@ class InlineEsbuildFunctionCode extends FunctionCode {
 /**
  * Get minify options.
  */
-function minifyOptions(minify: boolean = false) {
+function minifyOptions(minify: boolean = true) {
   if (!minify) {
     return {
       minify: false,
